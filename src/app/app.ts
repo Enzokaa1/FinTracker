e4c72ff2419aba2b0216c41fc754ed775b96e0be
Enzokaa1 <enzocastanhobb@gmail.com>
2025-12-24 03:50:51 -0300

    ajustes finais

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AF_Angular');
+  protected readonly title = signal('FrontFin');
 }
